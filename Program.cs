d996739667b74fca19aa72312062ce9d5cff04b0
DONGYUN HUANG <30042104@tafe.wa.edu.au>
2023-08-28 12:56:55 +0800

    blackHoleMass

--- Program.cs
+++ Program.cs
@@ -27,54 +27,57 @@ namespace MyClient
                 try
                 {
                     int option = int.Parse(Console.ReadLine());
-
                     try
                     {
                         switch (option)
                         {
                             case 1:
-                            // Input
-                            Console.WriteLine("Observed Wavelength:");
-                            double observedWavelength = double.Parse(Console.ReadLine());
-                            Console.WriteLine("Rest Wavelength:");
-                            double restWavelength = double.Parse(Console.ReadLine());
+                                // Input
+                                Console.WriteLine("Observed Wavelength:");
+                                double observedWavelength = double.Parse(Console.ReadLine());
+                                Console.WriteLine("Rest Wavelength:");
+                                double restWavelength = double.Parse(Console.ReadLine());
 
-                            // Output
-                            double velocity = calculate.StarVelocity(observedWavelength, restWavelength);
-                            Console.WriteLine($"Velocity Value: {velocity}");
-                            break;
-                        case 2:
-                            //Input
-                            Console.WriteLine("Arcseconds angle:");
-                            double arcsecondsAngle = double.Parse(Console.ReadLine());
-                            // Output
-                            double parsecs = calculate.StarDistance(arcsecondsAngle);
-                            Console.WriteLine($"Parsecs Value: {parsecs}");
-                            break;
-                        case 3:
-                            //Input
-                            Console.WriteLine("Celsius:");
-                            double celsius = double.Parse(Console.ReadLine());
-                            // Output
-                            double kelvin = calculate.TemperatureConversion(celsius);
-                            Console.WriteLine($"Kelvin Value: {kelvin}");
-                            break;
-                        case 4:
-                            //Input
-                            Console.WriteLine("Blackhole Mass:");
-                            double blackHoleMass = double.Parse(Console.ReadLine());
-                            // Output
-                            double schwarzschildRadius = calculate.BlackholeEventHorizon(blackHoleMass);
-                            Console.WriteLine($"SchwarzschildRadius Value: {schwarzschildRadius}");
-                            break;
-                        case 5:
-                            Console.WriteLine("Thanks for participating! Bye~ :)");
-                            isRunning = true;
-                            break;
-                        default:
-                            Console.WriteLine("More options are coming... ");
-                            break;
-                        }                       
+                                // Output
+                                double velocity = calculate.StarVelocity(observedWavelength, restWavelength);
+                                Console.WriteLine($"Velocity Value: {Math.Round(velocity, 4)}");
+                                break;
+                            case 2:
+                                //Input
+                                Console.WriteLine("Arcseconds angle:");
+                                double arcsecondsAngle = double.Parse(Console.ReadLine());
+                                // Output
+                                double parsecs = calculate.StarDistance(arcsecondsAngle);
+                                Console.WriteLine($"Parsecs Value: {parsecs}");
+                                //Console.WriteLine($"Parsecs Value: {Math.Round(parsecs, 4)}");
+                                break;
+                            case 3:
+                                //Input
+                                Console.WriteLine("Celsius:");
+                                double celsius = double.Parse(Console.ReadLine());
+                                // Output
+                                double kelvin = calculate.TemperatureConversion(celsius);
+                                Console.WriteLine($"Kelvin Value: {Math.Round(kelvin, 4)}");
+                                break;
+                            case 4:
+                                //Input
+                                Console.WriteLine("Blackhole Mass Base:");
+                                double massBase = double.Parse(Console.ReadLine());
+                                Console.WriteLine("Blackhole Mass Exponent:");
+                                double massExponent = double.Parse(Console.ReadLine());
+                                double blackHoleMass = massBase * Math.Pow(10, massExponent);
+                                // Output
+                                double schwarzschildRadius = calculate.BlackholeEventHorizon(blackHoleMass);
+                                Console.WriteLine($"SchwarzschildRadius Value: {schwarzschildRadius: 0.##E+00}"); // ** "string.Format()" VS string interpolation "$".
+                                break;
+                            case 5:
+                                Console.WriteLine("Bye, thanks for participating! :)");
+                                isRunning = true;
+                                break;
+                            default:
+                                Console.WriteLine("More options are coming... ");
+                                break;
+                        }
                     }
                     catch { Console.WriteLine("*** Please entry a double value ***"); }
                 }
